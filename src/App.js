b5a6e77d851a44b2a71e96da79878dff4bd9d6ec
Karan Muntode <143829123+karanmuntode@users.noreply.github.com>
2026-05-12 07:21:05 +0530

    Refactor App component and update imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import News from './News';
 
-import React, { Component } from 'react'
-import Navbar from './Components/Navbar';
-import News from './Components/News';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      category: 'business',
+      searchQuery: '',
+    };
+  }
 
-export default class App extends Component {
+  setCategory = (category) => {
+    this.setState({ category, searchQuery: '' });
+  }
+
+  setSearch = (query) => {
+    this.setState({ searchQuery: query, category: '' });
+  }
+
   render() {
     return (
       <div>
-      <Navbar/>
-      <News/>
-        
+        <Navbar setCategory={this.setCategory} setSearch={this.setSearch} />
+        <News
+          key={this.state.category + this.state.searchQuery}
+          category={this.state.category}
+          searchQuery={this.state.searchQuery}
+        />
       </div>
-    )
+    );
   }
 }
+
+export default App;
